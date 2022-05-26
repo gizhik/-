6b048a0f0d6b980b7d67146793eb5ca9e1600ba7
gizhik <106312935+gizhik@users.noreply.github.com>
2022-05-26 15:58:01 +0500

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name;
+int age;
+Console.WriteLine("Как вас зовут?");
+name = Console.ReadLine();
+Console.WriteLine("Сколько вам лет?");
+age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вас зовут - " + name + ". Вам " + age + " лет.");
